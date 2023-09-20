bdb9dcd9c0acb1e933dd667e86fe127bcafa0fab
Victor Hugo Z <victorhugo@offertech.com.br>
2023-09-20 14:19:14 -0300

    Primary Modified: logical changed

--- PBM/7 - Imagem RGB - média/p8.py
+++ PBM/7 - Imagem RGB - média/p8.py
@@ -1,20 +1,25 @@
 from PIL import Image
 
 
+def clear_folder(folder_name):
+    ...
+
+
 def convert_saturation(image_path):
     entrada = Image.open(image_path)
 
-    imagem_rgb = entrada.convert("RGB")
     largura, altura = entrada.size
+    nova_imagem = Image.new("P", (largura, altura))
 
-    for y in range(largura):
-        for x in range(altura):
-            r, g, b = imagem_rgb.getpixel((x, y))
+    for y in range(altura):
+        for x in range(largura):
+            r, g, b = entrada.getpixel((x, y))
             media_rgb = int((r + g + b) // 3)
-            entrada.putpixel((x, y), (media_rgb, media_rgb, media_rgb))
 
-    entrada.save("PBM/output/Fig4_media_rgb.ppm")
+            nova_imagem.putpixel((x, y), (media_rgb, media_rgb, media_rgb))
+
+    nova_imagem.save("PBM/output/Fig1_media_rgb", format="PPM")
 
 
 if __name__ == "__main__":
-    convert_saturation("PBM/assets/Fig4.ppm")
+    convert_saturation("PBM/assets/Fig1.ppm")
